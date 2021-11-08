ab8166d6b5148a6b716263aa3071642d89ae6686
Anwar Hossain <anwar.sarker@bjitgroup.com>
2021-11-08 09:06:56 +0600

    object Type

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import "./App.css";
 import { Greets } from "./components/Greets";
+import { Person } from "./components/Person";
 
 function App() {
+  const personName = {
+    first: "Mahedi",
+    last: "Hasan",
+  };
   return (
     <div className="App">
       <Greets name="Mahedi Hasan" messageCount={20} isLoggedIn={false} />
+      <Person name={personName} />
     </div>
   );
 }
