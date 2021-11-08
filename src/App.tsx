7df4edf167e1a1d0438c9e91faef87b4fb1999da
Anwar Hossain <anwar.sarker@bjitgroup.com>
2021-11-08 12:32:38 +0600

    Typing useRef hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import "./App.css";
 import { ThemeContextProvider } from "./components/context/ThemeContext";
 import { Box } from "./components/context/Box";
+import { DomRef } from "./components/refs/DomRef";
+import { MutableRef } from "./components/refs/MutableRef";
 
 function App() {
-  return <div className="App">
-    <ThemeContextProvider>
-      <Box />
-    </ThemeContextProvider>
-  </div>;
+  return (
+    <div className="App">
+      <ThemeContextProvider>
+        <Box />
+      </ThemeContextProvider>
+      <DomRef />
+      <MutableRef />
+    </div>
+  );
 }
 
 export default App;
