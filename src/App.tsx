4738f29215a203a9bddf4d627542840d0c2fd36a
Anwar Hossain <anwar.sarker@bjitgroup.com>
2021-11-08 09:31:52 +0600

    node/children prop type

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import { Heading } from "./components/Heading";
+import { Oscar } from "./components/Oscar";
 import { Status } from "./components/STatus";
 
 function App() {
@@ -7,6 +8,9 @@ function App() {
     <div className="App">
       <Status status="loading" />
       <Heading>Place Holder Text</Heading>
+      <Oscar>
+        <Heading>Oscar goes to leonardo Di Caprio</Heading>
+      </Oscar>
     </div>
   );
 }
