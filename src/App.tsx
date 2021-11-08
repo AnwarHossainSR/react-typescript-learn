2e4c23f5f45c7d7fafaa521ebd6eee1a3f94f71f
Anwar Hossain <anwar.sarker@bjitgroup.com>
2021-11-08 09:12:09 +0600

    Typing array

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,28 @@
 import "./App.css";
 import { Greets } from "./components/Greets";
 import { Person } from "./components/Person";
+import { PersonList } from "./components/PersonList";
 
 function App() {
   const personName = {
     first: "Mahedi",
     last: "Hasan",
   };
+  const nameList = [
+    {
+      first: "Jahid",
+      last: "Mahmud",
+    },
+    {
+      first: "SHakil",
+      last: "Ahmed",
+    },
+  ];
   return (
     <div className="App">
       <Greets name="Mahedi Hasan" messageCount={20} isLoggedIn={false} />
       <Person name={personName} />
+      <PersonList names={nameList} />
     </div>
   );
 }
