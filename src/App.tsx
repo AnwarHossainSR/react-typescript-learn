223e4e2ce985e748d3331c7d43af449a2e3f7657
Anwar Hossain <anwar.sarker@bjitgroup.com>
2021-11-08 10:39:17 +0600

    usestate in typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,10 @@
 import "./App.css";
-import { Button } from "./components/Button";
-import { Input } from "./components/Input";
+import { LoggedIn } from "./components/states/LoggedIn";
 
 function App() {
   return (
     <div className="App">
-      <Button
-        handleClick={(event, id) => console.log("event type", event, id)}
-      />
-      <Input value="dsf" handleChange={(e) => console.log(e.target.value)} />
+      <LoggedIn />
     </div>
   );
 }
