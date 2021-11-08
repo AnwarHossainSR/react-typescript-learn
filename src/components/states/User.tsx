f8d6abb0384f2b6092dcbb6f48b625068f53cbe7
Anwar Hossain <anwar.sarker@bjitgroup.com>
2021-11-08 10:51:27 +0600

    useState Type Assertion

--- src/components/states/User.tsx
+++ src/components/states/User.tsx
@@ -5,22 +5,18 @@ type authUser = {
   email: string;
 };
 export const User = () => {
-  const [user, setUser] = useState<authUser | null>(null);
+  const [user, setUser] = useState<authUser>({} as authUser);
   const handleLogin = () => {
     setUser({
       name: "Mahedi Hasan",
       email: "mahedianwar@Gmail.com",
     });
   };
-  const handleLogout = () => {
-    setUser(null);
-  };
   return (
     <div>
       <button onClick={handleLogin}>Login</button>
-      <button onClick={handleLogout}>Logout</button>
-      <div>User Name : {user?.name}</div>
-      <div>User EMail : {user?.email}</div>
+      <div>User Name : {user.name}</div>
+      <div>User EMail : {user.email}</div>
     </div>
   );
 };
